20917671426053d6039211a0d606ea4e4b89454c
SP-MICOM <superfamicom1022@gmail.com>
2025-12-12 22:52:24 +0900

    (2025.12.12)

--- Program/Source.c
+++ Program/Source.c
@@ -2,5 +2,61 @@
 
 void main()
 {
-	printf("Leegue of Legends");
+#pragma region 상술 연산자
+	//
+	//int storage = 0;
+	//
+	//int x = 10;
+	//int y = 5;
+	//int z = 1;
+	//int w = 10;
+	//
+	//const int constant = 5;
+	//
+	//// 1. 변수 = 변수 + 변수
+	//storage = x + y;
+	//
+	//printf("storage : %d\n", storage);
+	//
+	//
+	//// 2. 변수 = 변수 - 리터럴 상수
+	//storage = z - 1;
+	//
+	//printf("storage : %d\n", storage);
+	//
+	//// 3. 변수 = 변수 * 심볼릭 상수
+	//storage = w * constant;
+	//
+	//printf("storage : %d\n", storage);
+	//
+	//// 4. 변수 = 리터럴 상수 / 리터럴 상수
+	//storage = 20 / 5;
+	//
+	//printf("storage : %d\n", storage);
+	//
+	//// 5. 변수 = 심볼릭 상수 % 리터럴 상수
+	//storage = constant % 5;
+	//
+	//printf("storage : %d\n", storage);
+
+#pragma endregion
+
+#pragma region 비트
+	// 데이터를 나타내는 최소의 단위이며, 0 또는 1의
+	// 조합으로 논리 계산을 수행하는 단위입니다.
+
+#pragma region (10)진수를 (2)진수로 변환하는 과정
+	// 10진수를 1이 될 때까지 계속 2로 나누어 준 다음
+	// 나눈 위치의 나머지 값을 아래에서 위로 순서대로 정렬합니다.
+
+	// ex) 23 -> 10111
+#pragma endregion
+
+#pragma region (2)진수를 (10)진수로 변환하는 과정
+	// 1 byte에 2진수로 저장된 값을 2의 제곱으로 나타냅니다.
+
+	// 각각의 비트에 1이 있다면 1과 2의 제곱의 위치를 계산한
+	// 다음 각각의 비트를 모두 더하여 10진수로 나타냅니다.
+#pragma endregion
+
 }
