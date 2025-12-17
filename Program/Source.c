549e50f582d251c814c7d8393e4e85923eb38523
SP-MICOM <superfamicom1022@gmail.com>
2025-12-17 21:44:10 +0900

    (2025.12.17) 조건문

--- Program/Source.c
+++ Program/Source.c
@@ -84,16 +84,117 @@ void main()
 #pragma region switch문
 	// 어떤 결과에 따라 그 결과부터 실행되는 명령문입니다.
 
-	char grade = 'A';
+	// char grade = 'K';
 
-	switch (grade)
+	// switch문은 해당 조건이 완료되었을 때, break문이 없으면
+	// 나머지 밑에 있는 case 문까지 계속 실행시키다가 종료합니다.
+
+	// switch (grade)
+	// {
+	// case 'A': printf("100 ~ 90점\n");
+	// 	break;
+	// case 'B': printf("89 ~ 80점\n");
+	// 	break;
+	// case 'C': printf("79 ~ 70점\n");
+	// 	break;
+	// case 'D': printf("69 ~ 60점\n");
+	// 	break;
+	// case 'F': printf("60점 미만\n");
+	// 	break;
+	// default: printf("예외\n");
+	// 	break;
+	// }
+
+	// switch문의 경우 조건에 해당하는 값에 따라 조건의
+	// 위치로 이동합니다.
+
+#pragma endregion
+
+#pragma region 논리 연산자
+
+#pragma region AND 연산자
+// 두 개의 조건이 다 성립될 때 실행되는 연산자입니다.
+
+// int score = 75;
+// int attendacne = 80;
+// 	
+// if (score >= 60 && attendacne >= 80)
+// {
+// 	printf("Gradurate");
+// }
+
+// 조건문에서 하나 이상의 조건이 있다면 왼쪽부터 조건을 검사합니다.
+#pragma endregion
+
+#pragma region OR 연산자
+// 두 개의 조건 중에서 하나라도 조건이 성립될 때 실행되는 연산자입니다.
+
+// int weight = 70;
+// int bodyfat = 30;
+// 
+// if (weight >= 85 || bodyfat > 20)
+// {
+// 	printf("비만");
+// }
+
+// 조건문에 논리 표현식을 평가하는 도중에 결과가 이미 확정이 났다면, 그 이후의 평가는 생략합니다.
+#pragma endregion
+
+#pragma region NOT 연산자
+// 하나의 조건을 반전시키는 연산자입니다.
+
+// int power = 0;
+// 
+// if (!power)
+// {
+// 	printf("the power is ON");
+// }
+// else
+// {
+// 	printf("the power is OFF");
+// }
+
+#pragma endregion
+
+#pragma endregion
+
+#pragma region 사분면
+
+int x = 23;
+int y = 2;
+
+if (x >= 1 && y >= 1) // 1 사분면
+{
+	printf("제 1 사분면");
+}
+else if (x <= -1 && y >= 1) // 2 사분면
+{
+	printf("제 2 사분면");
+}
+else if (x <= -1 && y <= -1) // 3 사분면
+{
+	printf("제 3 사분면");
+}
+else if (x >= 1 && y <= -1) // 4 사분면
+{
+	printf("제 4 사분면");
+}
+else
+{
+	if (x == 0 && y != 0) // y 절편
 	{
-	case 'A': printf("100 ~ 90점입니다.");
-	case 'B': printf("89 ~ 80점입니다.");
-	case 'C': printf("79 ~ 70점입니다.");
-	case 'D': printf("69 ~ 60점입니다.");
-	case 'F': printf("60점 미만 점수입니다.");
+		printf("y 절편");
 	}
+	if (x != 0 && y == 0) // x 절편
+	{
+		printf("x 절편");
+	}
+	if (x == 0 && y == 0) // 0,0 원점
+	{
+		printf("원점");
+	}
+}
+
 
 #pragma endregion
 
